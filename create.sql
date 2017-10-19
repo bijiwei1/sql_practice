533be3fa4273c7f7828ff9042d5bcd20045abb70
cs143 <bibijijiweihch@gmail.com>
2017-10-18 23:27:11 -0700

    compiled

--- create.sql
+++ create.sql
@@ -1,22 +1,22 @@
 CREATE TABLE Employee(
-	person-name VARCHAR(10) NOT NULL,
+	personname VARCHAR(10) NOT NULL,
 	age INT,
 	street VARCHAR(10),
 	city VARCHAR(10)
 );
 
 CREATE TABLE Work(
-	person-name VARCHAR(10) NOT NULL,
-	company-name VARCHAR(10),
+	personname VARCHAR(10) NOT NULL,
+	companyname VARCHAR(10),
 	salary INT
 );
 
 CREATE TABLE Company(
-	company-name VARCHAR(10) NOT NULL,
+	companyname VARCHAR(10) NOT NULL,
 	city VARCHAR(10)
 );
 
 CREATE TABLE Manage(
-	person-name VARCHAR(10) NOT NULL,
-	manager-name VARCHAR(10)
+	personname VARCHAR(10) NOT NULL,
+	managername VARCHAR(10)
 );
